d43915d07a215600cba3ee884b1a2798d77bc927
Steven Hernandez <steven.hernandez.ctr@km-sscdev.org>
2024-01-24 14:44:14 -0800

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { ChangeEvent, useState, useEffect } from 'react';
 import { Button } from './components/button';
 import { Dialog } from './components/dialog';
 
-interface Scores {
+type ComplexityFactors = {
   technicalComplexity: { value: number, description: string };
   dependencies: { value: number, description: string };
   integrationDataMigration: { value: number, description: string };
@@ -17,7 +17,8 @@ interface Scores {
   crossFunctionalCoordination: { value: number, description: string };
   documentationKnowledgeTransfer: { value: number, description: string };
 }
-const defaultScores = {
+
+const defaultComplexityFactors: ComplexityFactors = {
   technicalComplexity: { value: 0, description: 'Technical Complexity: Consider the complexity of server-side logic, database interactions, APIs, data processing, and integration with other services or microservices. Assess the complexity related to UI/UX design, interactivity, state management, responsiveness, and client-side logic.' },
   dependencies: { value: 0, description: 'Dependencies: Evaluate dependencies within the project, such as modules, components, or services that the story depends on. Also consider third-party services, libraries, or APIs that the feature relies on and the potential risks or delays they might introduce.' },
   integrationDataMigration: { value: 0, description: 'Integration and Data Migration: Assess the effort required to integrate the new feature with existing parts of the application. Consider the complexity of any data migration or transformation that might be necessary.' },
@@ -32,23 +33,25 @@ const defaultScores = {
   documentationKnowledgeTransfer: { value: 0, description: 'Documentation and Knowledge Transfer: Include the effort required for documentation and knowledge transfer, especially for complex features that might impact multiple parts of the system.' }
 }
 
+const convertKeyToTitle = (key: string) => key.replace(/([A-Z])/g, ' $1').charAt(0).toUpperCase() + key.replace(/([A-Z])/g, ' $1').slice(1)
+
 const App: React.FC = () => {
-  const [scores, setScores] = useState<Scores>(defaultScores);
+  const [factors, setFactors] = useState<ComplexityFactors>(defaultComplexityFactors);
   const [description, setDescription] = useState('');
 
   const [result, setResult] = useState<string | null>(null);
   const [open, setOpen] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    setScores({ ...scores, [e.target.name]: { description: scores[e.target.name as keyof Scores].description, value: parseInt(e.target.value, 10) } });
+    setFactors({ ...factors, [e.target.name]: { description: factors[e.target.name as keyof ComplexityFactors].description, value: parseInt(e.target.value, 10) } });
   };
 
   useEffect(() => {
-    const totalScore = Object.values(scores).reduce((acc, curr) => acc + curr.value, 0);
+    const totalScore = Object.values(factors).reduce((acc, curr) => acc + curr.value, 0);
     const mappedScore = Math.round(totalScore / 3);
     setResult(`Total Score: ${totalScore}, Fibonacci Mapping: ${fibonacciMapping(mappedScore)}`);
-  }, [scores]);
-  
+  }, [factors]);
+
   const fibonacciMapping = (score: number): string => {
     if (score <= 1) return 'Trivial task (0-1)';
     if (score === 2) return 'Simple task (2)';
@@ -58,7 +61,7 @@ const App: React.FC = () => {
   };
 
   const resetValues = () => {
-    setScores(defaultScores);
+    setFactors(defaultComplexityFactors);
     setResult('');
   }
 
@@ -66,8 +69,8 @@ const App: React.FC = () => {
     <div className={'container max-lg mx-auto p-8 text-center'}>
       <h1 className="text-4xl my-2 font-bold">Story Complexity Rubric</h1>
       <p className="text-xl my-2">The purpose of this rubric is to facilitate communication around the pointing of a story using a collection of complexity factors that have been identified as common considerations across teams for evaluating story complexity. This rubric is meant to map to Fibonacci numbers (0, 1 ,2 ,3, 5, 8), with a maximum allowed complexity of an 8. </p>
-      
       <h2 className="text-2xl my-2 font-semibold">For each item below, select from the following options</h2>
+
       <ol>
         <li className='text-xl'><span className='font-semibold'>No Impact (0 Point):</span> The factor has no negative impact on the work in this story (ie, we have all the expertise we need for a story without additional research required)</li>
         <li className='text-xl'><span className='font-semibold'>Low (1 Point):</span> The factor has a minimal impact, or complexity is very low for this story.</li>
@@ -75,12 +78,12 @@ const App: React.FC = () => {
         <li className='text-xl'><span className='font-semibold'>High (3 Points):</span> The factor presents significant complexity or impact to the story.</li>
       </ol>
       <section>
-      {Object.keys(scores).map((score) => (
-          <div key={score} className='text-xl my-6'>
+        {Object.keys(factors).map((factor) => (
+          <div key={factor} className='text-xl my-6'>
             <label>
-              <QuestionMarkCircleIcon className="inline m-2 h-4" onClick={() => { setDescription(scores[score as keyof Scores].description); setOpen(true); }} />
-              {score.replace(/([A-Z])/g, ' $1').charAt(0).toUpperCase() + score.replace(/([A-Z])/g, ' $1').slice(1)}:
-              <select className='border' name={score} value={scores[score as keyof Scores].value} onChange={handleChange}>
+              <QuestionMarkCircleIcon className="inline m-2 h-4" onClick={() => { setDescription(factors[factor as keyof ComplexityFactors].description); setOpen(true); }} />
+              <span>{convertKeyToTitle(factor)}</span>:
+              <select className='border' name={factor} value={factors[factor as keyof ComplexityFactors].value} onChange={handleChange}>
                 <option value="0">No Impact</option>
                 <option value="1">Low</option>
                 <option value="2">Medium</option>
@@ -91,10 +94,10 @@ const App: React.FC = () => {
         ))}
         {/* <Button type="submit" className={'text-2xl'}>Calculate</Button> */}
         <Button type="button" onClick={resetValues} outline className={'text-2xl ml-4'}>Reset</Button>
-      {result && <p className={`text-2xl my-6 font-semibold ${result.includes('(8)') ? 'text-red-800' : 'text-green-600'}`} >{result}</p>}
-      <Dialog open={open} onClose={() => setOpen(false)} size='3xl' className={'rounded-xl p-10'}>
-        {description}
-      </Dialog>
+        {result && <p className={`text-2xl my-6 font-semibold ${result.includes('(8)') ? 'text-red-800' : 'text-green-600'}`} >{result}</p>}
+        <Dialog open={open} onClose={() => setOpen(false)} size='3xl' className={'rounded-xl p-10'}>
+          {description}
+        </Dialog>
       </section>
     </div>
   );
